fc5eeea3959872c47d46fabdb5f59ea98c7ca1f6
Suljo Sahbazovic <suljosahbazovic@gmail.com>
2026-07-06 15:17:40 +0200

    Child Elements

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -56,3 +56,14 @@ test('User facing locators', async ({page}) => {
   // by Title
   await page.getByTitle('IoT Dashboard').click() 
 })
+
+test('locating child elements', async ({ page }) => {
+  await page.locator('nb-card nb-radio :text-is("Option 1")').click()
+  await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+
+  // by button Sign in
+  await page.locator('nb-card').getByRole('button', { name: 'Sign in' }).first().click()
+
+  // by nth index child element
+  await page.locator('nb-card').nth(3).getByRole('button').click()
+})
