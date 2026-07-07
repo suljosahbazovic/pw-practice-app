8701e83b8961d6613ebf22c3c4449ac49f472edc
Suljo Sahbazovic <suljosahbazovic@gmail.com>
2026-07-07 16:07:51 +0200

    Parent Elements

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -67,3 +67,20 @@ test('locating child elements', async ({ page }) => {
   // by nth index child element
   await page.locator('nb-card').nth(3).getByRole('button').click()
 })
+
+test('locating parent elements', async ({ page }) => {
+  await page.locator('nb-card').getByText('Using the Grid').click()
+  await page.locator('nb-card', { hasText: 'Using the Grid' }).getByRole('textbox', { name: 'Email' }).click() 
+  await page.locator('nb-card', { has: page.locator('#inputEmail1')}).getByRole('textbox', { name: 'Email' }).click()
+
+  await page.locator('nb-card').filter({ hasText: 'Basic form' }).getByRole('textbox', { name: 'Email' }).click()
+  await page.locator('nb-card').filter({ has: page.locator('.status-danger')}).getByRole('textbox', { name: 'Password' }).click()
+
+  // find textbox Email insid nb-card that has checkbox and text 'Sign in' button
+  await page.locator('nb-card')
+            .filter({ has: page.locator('nb-checkbox') })
+            .filter({ hasText: 'Sign in' })
+            .getByRole('textbox', { name: 'Email' }).click()
+
+  await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', { name: 'Email' }).click()
+})
