f1cac5fdc1e604162be2dec2f5354ef08a45c36f
Suljo Sahbazovic <suljosahbazovic@gmail.com>
2026-07-08 16:46:16 +0200

    extracting values

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -98,3 +98,25 @@ test('Reusing the locators', async ({ page }) => {
 
   await expect(emailField).toHaveValue('test@test.com')
 })
+
+test('extracting values', async ({ page }) => {
+  // single text value
+  const basicForm = page.locator('nb-card').filter({ hasText: 'Basic form' })
+  const buttonText = await basicForm.locator('button').textContent()
+  expect(buttonText).toEqual('Submit')
+
+  // all text values
+  const allRadioButtonsLabels = await page.locator('nb-radio').allTextContents()
+  expect(allRadioButtonsLabels).toContain('Option 1')
+
+  // input value
+  const emailField = basicForm.getByRole('textbox', { name: 'Email' })
+  await emailField.fill('test@test.com')
+  const emailValue = await emailField.inputValue()
+  //console.log(emailValue)
+  expect(emailValue).toEqual('test@test.com')
+
+  // placeholder attribute value
+  const placeholderValue = await emailField.getAttribute('placeholder')
+  expect(placeholderValue).toEqual('Email')
+})
