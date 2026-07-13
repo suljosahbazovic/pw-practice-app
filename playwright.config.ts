315ad89e937c099cb8ddb389f65e1c9f065f14eb
Suljo Sahbazovic <suljosahbazovic@gmail.com>
2026-07-13 11:17:45 +0200

    Input Fields

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,8 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 10000,
-  globalTimeout: 60000,
+  //timeout: 10000,
+  //globalTimeout: 60000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -32,8 +32,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     //actionTimeout: 5000,
-    navigationTimeout: 5000,
-    headless: true,
+    //navigationTimeout: 5000,
+    headless: false,
     trace: 'on-first-retry',
   },
 
