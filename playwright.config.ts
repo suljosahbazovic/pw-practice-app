90ff16aa2c615ae245730bcce287b5509ca56adb
Suljo Sahbazovic <suljosahbazovic@gmail.com>
2026-07-21 12:15:25 +0200

    Run Playwright in headless mode in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     //actionTimeout: 5000,
     //navigationTimeout: 5000,
-    headless: false,
+    //headless: false,
     trace: 'on-first-retry',
   },
 
