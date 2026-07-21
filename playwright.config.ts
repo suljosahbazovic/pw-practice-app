199b66961ecd729395c18d9452087ae6361dc33f
Suljo Sahbazovic <suljosahbazovic@gmail.com>
2026-07-21 13:48:12 +0200

    Fix Playwright Config File

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -76,9 +77,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:4200',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
