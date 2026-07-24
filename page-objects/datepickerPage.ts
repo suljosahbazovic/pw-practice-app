8222b636211afc8c2bce48748c19102836668a57
Suljo Sahbazovic <suljosahbazovic@gmail.com>
2026-07-24 18:33:15 +0200

    Fix HelperBase

--- page-objects/datepickerPage.ts
+++ page-objects/datepickerPage.ts
@@ -1,11 +1,12 @@
 import { Page, expect } from '@playwright/test'
 import { HelperBase } from './helperBase'
 
-export class DatePickerPage extends HelperBase{
-    //private readonly page: Page
+export class DatePickerPage{
+    private readonly page: Page
 
     constructor (page: Page){
-        super(page)
+        //super(page)
+        this.page = page
     }
 
     async selectCommonDatePickerDateFromToday(numberOfDaysFromToday: number){
--- page-objects/formLayoutsPage.ts
+++ page-objects/formLayoutsPage.ts
@@ -1,15 +1,17 @@
 import { Page } from '@playwright/test'
 import { HelperBase } from './helperBase'
 
-export class FormLayoutsPage extends HelperBase{
-    //private readonly page: Page
+export class FormLayoutsPage{
+    private readonly page: Page
 
     constructor(page: Page){
-        super(page)
+        this.page = page
+        //super(page)
     }
     
     async submitUsingTheGridFormWithCredentialsAndSelectOption(email: string, password: string, optionText: string){
         const usingTheGridForm = this.page.locator('nb-card', { hasText: 'Using the Grid' })
+        //await this.waitForNumberOfSeconds(2)
         await usingTheGridForm.getByRole('textbox', { name: "Email" }).fill(email)
         await usingTheGridForm.getByRole('textbox', { name: "Password" }).fill(password)
         await usingTheGridForm.getByRole('radio', { name: optionText }).check( {force: true})
