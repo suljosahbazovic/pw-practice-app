d9859f10d086274942068f560120626fc98aec21
Suljo Sahbazovic <suljosahbazovic@gmail.com>
2026-07-28 15:25:32 +0200

    Screenshots and Videos Capturing

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,11 +17,11 @@ export default defineConfig({
   //globalTimeout: 60000,
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -36,6 +36,11 @@ export default defineConfig({
     //navigationTimeout: 5000,
     //headless: false,
     trace: 'on-first-retry',
+    video: 'on'
+    // {
+    //   mode: 'on',
+    //   size: {width: 1920, height: 1080}
+    // }
   },
 
   /* Configure projects for major browsers */
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -22,7 +22,11 @@ test('parametrized methods', async ({ page }) => {
 
   await pm.navigateTo().formLayoutsPage()
   await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'test', 'Option 2')
+  await page.screenshot({ path: 'screenshots/formLayoutsPage.png' });
+  const buffer = await page.screenshot()
+  console.log(buffer.toString('base64'))
   await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckBox(randomFullName, randomEmail, false)
+  await page.locator('nb-card', { hasText: 'Inline form' }).screenshot({ path: 'screenshots/inlineForm.png' });
   await pm.navigateTo().datepickerPage()
   await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(5)
   await pm.onDatePickerPage().selectDatePickerWithRangeFromToday(6, 10)
