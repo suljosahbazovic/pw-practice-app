0087ac9a8d0f287960971ecc2b8d54b1db45bfc3
Suljo Sahbazovic <suljosahbazovic@gmail.com>
2026-07-29 11:53:11 +0200

    Test Fixtures

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -22,12 +22,12 @@ test('parametrized methods', async ({ page }) => {
 
   await pm.navigateTo().formLayoutsPage()
   await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption(process.env.USERNAME!, process.env.PASSWORD!, 'Option 2')
-  await page.screenshot({ path: 'screenshots/formLayoutsPage.png' });
-  const buffer = await page.screenshot()
-  console.log(buffer.toString('base64'))
+  // await page.screenshot({ path: 'screenshots/formLayoutsPage.png' });
+  // const buffer = await page.screenshot()
+  // console.log(buffer.toString('base64'))
   await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckBox(randomFullName, randomEmail, false)
-  await page.locator('nb-card', { hasText: 'Inline form' }).screenshot({ path: 'screenshots/inlineForm.png' });
-  await pm.navigateTo().datepickerPage()
-  await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(5)
-  await pm.onDatePickerPage().selectDatePickerWithRangeFromToday(6, 10)
+  // await page.locator('nb-card', { hasText: 'Inline form' }).screenshot({ path: 'screenshots/inlineForm.png' });
+  // await pm.navigateTo().datepickerPage()
+  // await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(5)
+  // await pm.onDatePickerPage().selectDatePickerWithRangeFromToday(6, 10)
 })
